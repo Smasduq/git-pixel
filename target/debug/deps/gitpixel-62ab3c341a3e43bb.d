30c89bc1ed97ce15803f189141ee0178d3e80984
Smasduq <sadiqulmasduq2010@gmail.com>
2026-09-03 02:36:15 +0100

    third commit

--- target/debug/deps/gitpixel-62ab3c341a3e43bb.d
+++ target/debug/deps/gitpixel-62ab3c341a3e43bb.d
@@ -1,6 +1,7 @@
-/home/smasduq/Projects/gitpixel/target/debug/deps/gitpixel-62ab3c341a3e43bb.d: src/lib.rs src/calendar.rs
+/home/smasduq/Projects/gitpixel/target/debug/deps/gitpixel-62ab3c341a3e43bb.d: src/lib.rs src/calendar.rs src/font.rs
 
-/home/smasduq/Projects/gitpixel/target/debug/deps/gitpixel-62ab3c341a3e43bb: src/lib.rs src/calendar.rs
+/home/smasduq/Projects/gitpixel/target/debug/deps/gitpixel-62ab3c341a3e43bb: src/lib.rs src/calendar.rs src/font.rs
 
 src/lib.rs:
 src/calendar.rs:
+src/font.rs:
